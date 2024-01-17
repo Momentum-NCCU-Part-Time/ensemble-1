a6bcc0a1ef771782b7cbb5c863cbb3a02dd5ca7f
Jarrod <gatejarr@gmail.com>
2024-01-16 20:58:13 -0500

    WIP; we were SO CLOSE.

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const port = 3000;
 
 const app = express();
 
-app.use(morgan("tiny"))
+const fs = require("fs").promises;
+
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World 2!</h1>")
-})
+  res.send("<h1>Hello World 2!</h1>");
+  fs.readFile("./fortunes.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(`Error reading file from disk: ${err}`);
+    } else {
+      // parse JSON string to JSON object
+      const fortunes = JSON.parse(data);
 
-app.get("/hello/", (req,res) => {
-    res.json({message: "Hello World from /hello!", anotherThing: "I love dogs"})
-})
+      // print all databases
+      fortunes.forEach((db) => {
+        console.log(["fortunes"]);
+        res.json({ fortune: fortunes });
+      });
+    }
+  });
+});
+
+app.get("/fortunes/", (req, res) => {
+  res.json({
+    message: "fortune:",
+    anotherThing: "I love dogs",
+  });
+});
 
 // // can replace :name with :lang
 // app.get("/hello/:name", (req, res) => {
@@ -20,4 +39,6 @@ app.get("/hello/", (req,res) => {
 //     res.json({message: 'Hello to you, ${req.params.name}!'})
 // })
 
-app.listen(port, () => { console.log('Server is running on port ${port}') })
+app.listen(port, () => {
+  console.log("Server is running on port ${port}");
+});

5efda480e42674322577243131388ac64b33d371
Poonam Pande <p.pande@earthlink.net>
2024-01-18 19:26:46 -0500

    hand off

--- index.js
+++ index.js
@@ -4,18 +4,27 @@ const port = 3000;
 
 const app = express();
 
-const fs = require("fs").promises;
+// const fs = require("fs").promises;
 
-app.use(morgan("tiny"));
+const fortunes = [
+  "Embrace the learning journey. Coding experience is built step by step.",
+  "Your dedication to learning to code will open doors to new opportunities.",
+  "Just like debugging, challenges are temporary. Keep coding!",
+  "In the world of tech, every line of code you write is a step toward success.",
+  "Your career switch into tech is like a well-optimized algorithm: efficient and rewarding.",
+  "Every error is an opportunity to learn something new. Don't give up!"
+]
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World 2!</h1>");
-  fs.readFile("./fortunes.json", "utf8", (err, data) => {
-    if (err) {
-      console.log(`Error reading file from disk: ${err}`);
-    } else {
+app.use(morgan("dev"));
+
+app.get("/api/fortunes/", (req, res) => {
+  res.send(`${fortunes}`);
+  // fs.readFile("./fortunes.json", "utf8", (err, data) => {
+  //   if (err) {
+  //     console.log(`Error reading file from disk: ${err}`);
+  //   } else {
       // parse JSON string to JSON object
-      const fortunes = JSON.parse(data);
+      // const fortunes = JSON.parse(data);
 
       // print all databases
       fortunes.forEach((db) => {
@@ -23,8 +32,8 @@ app.get("/", (req, res) => {
         res.json({ fortune: fortunes });
       });
     }
-  });
-});
+  );
+;
 
 app.get("/fortunes/", (req, res) => {
   res.json({
